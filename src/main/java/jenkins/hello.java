95bc2dc2c894e424cd457fa28b4aeb5c0ff7d26f
wangkai <980536525@qq.com>
2019-10-15 18:57:27 +0800

    555

--- src/main/java/jenkins/hello.java
+++ src/main/java/jenkins/hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
     @RequestMapping("/hello")
     public String test1(){
-        return "jenkins";
+        return "jenkins+111";
     }
 
 }
