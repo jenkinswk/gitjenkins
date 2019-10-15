eed3559b1050c51034a898235185c96c2725f2b0
wangkai <980536525@qq.com>
2019-10-15 10:23:39 +0800

    1111

---
+++ src/main/java/jenkins/hello.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @RequestMapping("/hello")
+    public String test1(){
+        return "jenkins";
+    }
+
+}
